2d507451f831d73266714101301cf4bc2cf9c3eb
nilesh-topcoder <41110540+nilesh-topcoder@users.noreply.github.com>
2018-08-01 18:43:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const staticHref = '/static';
+const publicFolder = path.join(__dirname, '/');
+
+app.use(staticHref, express.static(publicFolder));
+app.use(staticHref, function(req, res) {
+    res.send(404);
+});
+
+app.all('/*', function(req, res) {
+    res.sendFile('index.html', {root: publicFolder});
+});
+
+app.listen(3000, function(){
+    console.log('running');
+	});
